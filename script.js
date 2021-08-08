4c0eca60cd0befd7546f2238cb1eeb1f211e2918
edumt <edumoura1997@gmail.com>
2021-08-08 15:57:02 -0300

    added score + fixed out of bounds bug

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
 let context = canvas.getContext("2d"); //....
+let score = document.getElementById("score");
 let box = 32;
 canvas.width = 16 * box;
 canvas.height = 16 * box;
@@ -32,6 +33,26 @@ function drawFood() {
   context.fillRect(food.x, food.y, box, box);
 }
 
+function getPoints(length) {
+  let points;
+  if (length < 11) points = 5;
+  else if (length < 20) points = 10;
+  else if (length < 30) points = 20;
+  else points = 30;
+  return points;
+}
+
+function spawnFood() {
+  food.x = Math.floor(Math.random() * 16) * box;
+  food.y = Math.floor(Math.random() * 16) * box;
+  for (i = 0; i < snake.length; i++) {
+    if (food.x == snake[i].x && food.y == snake[i].y) {
+      return false;
+    }
+  }
+  return true;
+}
+
 //quando um evento acontece, detecta e chama uma função
 document.addEventListener("keydown", update);
 
@@ -43,10 +64,10 @@ function update(event) {
 }
 
 function iniciarJogo() {
-  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+  if (snake[0].x > 15 * box && lastDirection == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && lastDirection == "left") snake[0].x = 15 * box;
+  if (snake[0].y > 15 * box && lastDirection == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && lastDirection == "up") snake[0].y = 15 * box;
 
   for (i = 1; i < snake.length; i++) {
     if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
@@ -68,8 +89,8 @@ function iniciarJogo() {
   if (direction == "down") snakeY += box;
 
   if (snakeX == food.x && snakeY == food.y) {
-    food.x = Math.floor(Math.random() * 16) * box;
-    food.y = Math.floor(Math.random() * 16) * box;
+    score.textContent = Number(score.textContent) + getPoints(snake.length);
+    while (!spawnFood()); //keeps trying to spawn food outside the snake's body until it succeeds
   } else {
     snake.pop();
   }
